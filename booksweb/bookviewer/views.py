bd44b0f9880df90a5072c5c03873783b97daa147
AndresN <camilonunezz9812@gmail.com>
2021-08-24 22:41:22 -0500

    booklist style added

--- booksweb/bookviewer/views.py
+++ booksweb/bookviewer/views.py
@@ -5,16 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     books_list = list(Book.objects.all().values())
-    html = ''
-    header = """
-        <h1>Books Viewer</h1>
-    """
-    for book in books_list:
-        html += "<ul>"
-        html+=f"<li>{book['name']}</li>"
-        html+=f"<li>{book['author_name']}</li>"
-        html+=f"<li>{book['category']}</li>"
-        html+=f"<li>{book['isbn']}</li>"
-        html+=f"<li>{book['publication_date']}</li>"
-        html += "</ul>"
-    return HttpResponse(html)
+    return render(request, 'index.html',{
+        'books_list': books_list,
+    })
